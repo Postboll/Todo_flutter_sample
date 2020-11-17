c57be798a0a7882260c5ef2aa790571cd3dff22d
Postboll <ro1228@icloud.com>
2020-11-18 08:53:33 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app_test/main_model.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel()..getTodoList(),
+        child: Scaffold(
+          appBar: AppBar(
+
+           title: Text("Todoapps"),
+           ),
+
+           body: Consumer<MainModel>(builder: (context,model, child) {
+             final todoList = model.todoList;
+
+
+             return ListView(
+               children: todoList
+                   .map((todo) =>
+                   ListTile(
+                     title: Text(todo.title),
+                   ),
+               ).toList(),
+             );
+           }),
+
+               floatingActionButton: FloatingActionButton(
+
+                     tooltip: 'Increment',
+                      child: Icon(Icons.add),
+               ), // This trailing comma makes auto-formatting nicer for build methods.
+                 ),
+      ),
+          );
+  }
+}
+
