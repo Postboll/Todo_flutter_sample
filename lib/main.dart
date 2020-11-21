9e1e166ae6fadae97aedc540d43361c710679f75
Postboll <ro1228@icloud.com>
2020-11-21 10:56:19 +0900

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:todo_app_test/main_model.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
@@ -15,42 +14,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-
-
     return MaterialApp(
       title: 'Flutter Demo',
-
       home: ChangeNotifierProvider<MainModel>(
         create: (_) => MainModel()..getTodoListRealTime(),
         child: Scaffold(
           appBar: AppBar(
+            title: Text("Todoapps"),
+          ),
 
-           title: Text("Todoapps"),
-           ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            final todoList = model.todoList;
 
-           body: Consumer<MainModel>(builder: (context,model, child) {
-             final todoList = model.todoList;
+            return ListView(
+              children: todoList
+                  .map(
+                    (todo) => ListTile(
+                      title: Text(todo.title),
+                    ),
+                  )
+                  .toList(),
+            );
+          }),
 
-
-             return ListView(
-               children: todoList
-                   .map((todo) =>
-                   ListTile(
-                     title: Text(todo.title),
-                   ),
-               ).toList(),
-             );
-           }),
-
-               floatingActionButton: FloatingActionButton(
-
-                     tooltip: 'Increment',
-                      child: Icon(Icons.add),
-               ), // This trailing comma makes auto-formatting nicer for build methods.
-                 ),
+          floatingActionButton: FloatingActionButton(
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
       ),
-          );
+    );
   }
 }
-
